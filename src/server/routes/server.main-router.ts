7cd580fe232b6f31435cc4a1cab6a8973dd04825
Sn0wFox <ruben.pericas@laposte.net>
2017-02-11 16:44:40 +0100

    Fixed routing bug: querying directly angular's routes in the browser now works fine

--- src/server/routes/server.main-router.ts
+++ src/server/routes/server.main-router.ts
@@ -1,14 +1,21 @@
-import * as express from "express";
-import * as path from "path";
+import * as express from 'express';
+import * as path    from 'path';
 
-export const mainRouter: any = express.Router();
-// NOTE: the type 'any' fixes a bug between WebStorm and @Types
+const baseFolder: string          = path.resolve(__dirname + '/../../client');
+const mainRouter: express.Router  = express.Router();
 
 // Serve static files
-mainRouter.use(express.static(path.resolve(__dirname + "/../../client")));
+mainRouter.use(express.static(baseFolder));
 
-// Server a custom 404 page in case of an unknown route
-mainRouter.get("*", (req: any, res: any) => {
+// Match angular routes
+function handleAngularRoutes(req: express.Request, res: express.Response) {
+  res.status(200).sendFile('index.html', { root: baseFolder });
+}
+mainRouter.get('/about', handleAngularRoutes);
+mainRouter.get('/home', handleAngularRoutes);
+
+// Serve a custom 404 page in case of an unknown route
+mainRouter.all('*', (req: express.Request, res: express.Response) => {
   res.status(404).json({status: 404, message: "Resource not found"});
 });
 
