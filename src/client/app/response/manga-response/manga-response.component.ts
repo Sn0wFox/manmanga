ab11ff8c28a97ae83ce657558684d1a124043efd
Sn0wFox <ruben.pericas@laposte.net>
2017-02-23 17:33:30 +0100

    Cleaned manga response component

--- src/client/app/response/manga-response/manga-response.component.ts
+++ src/client/app/response/manga-response/manga-response.component.ts
@@ -1,31 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Manga }  from '../../../../lib/interfaces/manga.interface';
 
-const MANGATEST: Manga = {
-  title: 'Death Note',
-  author: {name: 'Tsugumi Oba'},
-  illustrator: ['Takeshi Obata'],
-  volumes: 12,
-  genres: ['Thriller', 'Drama'],
-  abstract: 'Kira kill the world with a book from hell',
-  coverUrl: 'http://mcd.iosphe.re/n/41/1/front/a/'
-};
+// const MANGATEST: Manga = {
+//   title: 'Death Note',
+//   author: {name: 'Tsugumi Oba'},
+//   illustrator: ['Takeshi Obata'],
+//   volumes: 12,
+//   genres: ['Thriller', 'Drama'],
+//   abstract: 'Kira kill the world with a book from hell',
+//   coverUrl: 'http://mcd.iosphe.re/n/41/1/front/a/'
+// };
 
 @Component({
   selector: 'mmg-manga-response',
   templateUrl: 'manga-response.component.pug',
   styleUrls: ['manga-response.component.scss']
 })
-export class MangaComponent implements OnInit {
-  @Input() manga: Manga;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    // // console.log('Manga !');
-  }
-  setManga(manga: Manga): void {
-    this.manga = manga;
-  }
+export class MangaComponent {
+  @Input()
+  manga: Manga;
 }
