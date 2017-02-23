4bf045c9c9c7fce2744c9ed617789a1a76800d89
Sn0wFox <ruben.pericas@laposte.net>
2017-02-23 17:49:47 +0100

    Styled anime response component

--- src/client/app/response/anime-response/anime-response.component.ts
+++ src/client/app/response/anime-response/anime-response.component.ts
@@ -1,24 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Anime }  from '../../../../lib/interfaces/anime.interface';
-
-const ANIMETEST: Anime = {
-  title: 'One piece'
-};
 
 @Component({
   selector: 'mmg-anime-response',
   templateUrl: 'anime-response.component.pug',
   styleUrls: ['anime-response.component.scss']
 })
-export class AnimeComponent implements OnInit {
-  @Input() anime: Anime;
-
-  ngOnInit(): void {
-    // console.log('Anime !');
-  }
-
-  setAnime(anime: Anime): void {
-    this.anime = anime;
-  }
+export class AnimeComponent {
+  @Input()
+  anime: Anime;
 }
